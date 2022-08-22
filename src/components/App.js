01db52d5262a5b7b4a01af6f3d8a42ae7ef26505
sencer2022 <senceralpdogdu@gmail.com>
2022-08-22 11:45:53 +0300

    Added Users component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import Navbar from './Navbar';
 import User from './User';
+import Users from './Users';
 
 export class App extends Component {
   render() {
     return ( // Kapsayıcı elaman olarak boş yere <div> kullanmak yerine <React.Fragment> yada <Fragment> ya da <> kullanılır.
         <>
           <Navbar />
-          <User />
+          <Users />
         </>
     )
   }
