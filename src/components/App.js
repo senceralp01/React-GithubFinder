5ebb2b16242bb33a801b8db85cac1449e11db60e
sencer2022 <senceralpdogdu@gmail.com>
2022-08-22 12:47:06 +0300

    Added Http request via axios

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,29 @@ import React, { Component, Fragment } from 'react'
 import Navbar from './Navbar';
 import User from './User';
 import Users from './Users';
+import axios from 'axios';
 
 export class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users')
+      .then(response => this.setState({users: response.data}))
+  }
+
   render() {
     return ( // Kapsayıcı elaman olarak boş yere <div> kullanmak yerine <React.Fragment> yada <Fragment> ya da <> kullanılır.
-        <>
-          <Navbar />
-          <Users />
-        </>
+      <>
+        <Navbar />
+        <Users users={this.state.users} />
+      </>
     )
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,55 +2,12 @@ import React, { Component } from 'react';
 import User from './User';
 
 class Users extends Component {
-    constructor(props) {
-        super(props); // super olarak ilgili base'e props'u gönderdik.
-        this.state = {
-            users: [
-                {
-                    login: "technoweenie",
-                    id: 21,
-                    avatar_url: "https://avatars.githubusercontent.com/u/21?v=4",
-                    html_url: "https://github.com/technoweenie"
-                },
-                {
-                    login: "macournoyer",
-                    id: 22,
-                    avatar_url: "https://avatars.githubusercontent.com/u/22?v=4",
-                    html_url: "https://github.com/macournoyer"
-                },
-                {
-                    login: "takeo",
-                    id: 23,
-                    avatar_url: "https://avatars.githubusercontent.com/u/23?v=4",
-                    html_url: "https://github.com/takeo"
-                },
-                {
-                    login: "technoweenie",
-                    id: 21,
-                    avatar_url: "https://avatars.githubusercontent.com/u/21?v=4",
-                    html_url: "https://github.com/technoweenie"
-                },
-                {
-                    login: "macournoyer",
-                    id: 22,
-                    avatar_url: "https://avatars.githubusercontent.com/u/22?v=4",
-                    html_url: "https://github.com/macournoyer"
-                },
-                {
-                    login: "takeo",
-                    id: 23,
-                    avatar_url: "https://avatars.githubusercontent.com/u/23?v=4",
-                    html_url: "https://github.com/takeo"
-                }
-            ]
-        }
-    }
     render() {
         return (
             <div className='container mt-3'>
                 <div className="row">
-                    {this.state.users.map(user => (
-                        <User user={user} />
+                    {this.props.users.map(user => (
+                        <User user={user} key={user.id} />
                     ))}
                 </div>
             </div>
